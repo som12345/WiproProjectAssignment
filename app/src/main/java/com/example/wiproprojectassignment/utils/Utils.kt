8911adb97fdc39e240a91871472f1d14c0622b3a
som12345 <soumya.ranjan407@gmail.com>
2020-10-25 02:40:48 +0530

    FFINAL CODE PUSH ----------------------------NO MORE PUSH

--- app/src/main/java/com/example/wiproprojectassignment/utils/Utils.kt
+++ app/src/main/java/com/example/wiproprojectassignment/utils/Utils.kt
@@ -6,6 +6,7 @@ import android.net.NetworkCapabilities
 import android.os.Build
 
 object Utils {
+
     fun checkInternet(context: Context): Boolean {
         if (context == null) return false
         val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
