828e11ba0283bea2cd47ba1e97dcaf510c694f73
som12345 <soumya.ranjan407@gmail.com>
2020-10-25 02:13:08 +0530

    INTERNET CHECK ADDED AND FINAL COMMIT

---
+++ app/src/main/java/com/example/wiproprojectassignment/ui/WiproDataShowFragment.kt
@@ -0,0 +1,174 @@
+package com.example.wiproprojectassignment.ui
+
+import android.app.AlertDialog
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.wiproprojectassignment.R
+import com.example.wiproprojectassignment.utils.Utils
+
+
+class WiproDataShowFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
+
+    private val model by lazy { ViewModelProvider(this).get(WiproCountryInfoListViewModel::class.java) }
+    private var swipeRefreshLayout: SwipeRefreshLayout? = null
+    private var recyclerView: RecyclerView? = null
+    private var adapterWipro: WiproCountryInfoListAdapter? = null
+    private lateinit var dialog: ProgressDialog
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var view =  inflater.inflate(R.layout.fragment_wipro_data_show, container, false)
+        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        swipeRefreshLayout?.setOnRefreshListener(this)
+        observeField()
+        callApi()
+        return view
+    }
+
+    private fun callApi() {
+        if (activity?.let { Utils.checkInternet(it) } == true) {
+            model.fetchData()
+        } else {
+            showAlert()
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(activity)
+        with(builder)
+        {
+            setTitle("Please check your internet!")
+            setMessage("No internet")
+            setPositiveButton(
+                "OK",
+                DialogInterface.OnClickListener(function = positiveButtonClick as (DialogInterface, Int) -> Unit)
+            )
+            show()
+        }
+    }
+
+    val positiveButtonClick = { dialog: DialogInterface, which: Int ->
+       activity?.finish()
+    }
+
+    /**
+     * Check internet
+     */
+    private fun isNetworkAvailable(): Boolean {
+        if (context == null) return false
+        val connectivityManager = activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = WiproDataShowFragment()
+    }
+
+    /**
+     * Method to observe API response
+     *
+     */
+    private fun observeField() {
+        activity?.let {
+            model.list.observe(
+                it,
+                Observer {
+                    Log.d("result", it.title)
+                    adapterWipro = WiproCountryInfoListAdapter(activity!!, it.rows)
+                    val mLayoutManager: RecyclerView.LayoutManager =
+                        LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+                    recyclerView!!.layoutManager = mLayoutManager
+                    recyclerView!!.itemAnimator = DefaultItemAnimator()
+                    recyclerView!!.setHasFixedSize(true)
+                    recyclerView!!.adapter = adapterWipro
+                    swipeRefreshLayout!!.isRefreshing = false
+                })
+        }
+
+        activity?.let {
+            model.toastMsg.observe(
+                it,
+                Observer {
+                    Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+                })
+        }
+
+        activity?.let {
+            model.showLoader.observe(it,
+                Observer {
+                    if (it) showLoading() else hideLoading()
+                }
+            )
+        }
+    }
+
+    /**
+     * Show dialog
+     */
+    private fun showLoading() {
+        dialog = ProgressDialog(activity).apply {
+            setCanceledOnTouchOutside(false)
+            setMessage("Please wait...")
+        }
+        if (!dialog.isShowing) {
+            dialog.show()
+        }
+    }
+
+    /**
+     * Hide dialog
+     */
+    private fun hideLoading() {
+        if (dialog.isShowing) dialog.dismiss()
+    }
+
+    override fun onRefresh() {
+        callApi()
+    }
+}
